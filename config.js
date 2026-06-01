0130fe5346e8e552fcf3af1aba1ecd21e6f86714
cbell003 <cbell03@uoregon.edu>
2026-05-31 17:00:37 -0700

    Update

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ var config = {
             alt: 'Map of the Great Basin in the USA',
             description: ' Source: Kmusser, By <a href="//commons.wikimedia.org/wiki/User:Kmusser" title="User:Kmusser">Kmusser</a> at <a href="//commons.wikimedia.org/wiki/Main_Page" title="Main Page"> Wikimedia Commons</a>, <a href="https://creativecommons.org/licenses/by-sa/3.0" title="Creative Commons Attribution-Share Alike 3.0">CC BY-SA 3.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=117776104">Link</a> <br><br>  The Paisley Caves, located in Oregon and the Great Basin, are a significant archaeological site that has provided insight into the paleoenvironment of the Great Basin and human occupation during the Pleistocene and Early Holocene. ',
             location: {
-                center: [-120.5579, 42.7529],
+                center: [-120.5527, 42.7072],
                 pitch: 10,
                 zoom: 5.5,
                 bearing: 0,
@@ -53,7 +53,7 @@ var config = {
             title: ' Important findings at the Paisley Caves',
             description: 'Great Basin, because what has been found there challenges previous ideas of how and when people initially arrived into what is now North America. The main evidence found at Paisley Caves that show that people arrived earlier than expected, is found through coprolites and western stemmed projectile points. Before the discoveries found at the Paisley Caves, it was thought that clovis point technology was the first to arrive in North America, and that people arrived around 13,000 years ago through the Ice Free Corridor.  ',
             location: {
-                center: [-120.557, 42.752],
+                center: [-120.5527, 42.7072],
                 zoom: 6.5,
                 pitch: 60,
                 bearing: -53.2,
@@ -78,9 +78,9 @@ var config = {
             image: 'https://upload.wikimedia.org/wikipedia/commons/8/8d/Western_Stemmed_Tradition_points.png',
             alt: 'Western Stemmed projectile points ',
             imageSize: 'small',
-            description: 'Source: Thomas Brown,Daniel McGowan Gilmour, Paul S. Solimano, Kenneth Ames, <a rel="nofollow" class="external free" href="https://pdxscholar.librardxy.p.edu/anth_fac/201/">https://pdxscholar.library.pdx.edu/anth_fac/201/</a> The Radiocarbon Record of the Western Stemmed Tradition on the Southern Columbia Plateau of Western North America, <a href="https://creativecommons.org/licenses/by/4.0" title="Creative Commons Attribution 4.0">CC BY 4.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=149052877">Link</a> <br><br>  Western Stemmed projectile points were an important find at the Paisley Caves because they helped to challenge previous assumptions about early human migration and technology in North America and the Great Basin. The projectile points found were relatively dated to about 11,070 and 11,340 radio carbon years which would make them as old or older than discovered Clovis technology. Due to the age of the Western Stemmed projectile points, it means that humans entered what is now North America before the Ice Free corridor was open. This means that people likely arrived and made their way through North America by moving along the Pacific Coast. The age of the Western Stemmed projectile points also means that Western Stemmed technology and Clovis technology developed independently from each other and are not connected.',
+            description: 'Source: Thomas Brown,Daniel McGowan Gilmour, Paul S. Solimano, Kenneth Ames, <a rel="nofollow" class="external free" href="https://pdxscholar.library.pdx.edu/anth_fac/201/">https://pdxscholar.library.pdx.edu/anth_fac/201/</a> The Radiocarbon Record of the Western Stemmed Tradition on the Southern Columbia Plateau of Western North America, <a href="https://creativecommons.org/licenses/by/4.0" title="Creative Commons Attribution 4.0">CC BY 4.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=149052877">Link</a> <br><br>  Western Stemmed projectile points were an important find at the Paisley Caves because they helped to challenge previous assumptions about early human migration and technology in North America and the Great Basin. The projectile points found were relatively dated to about 11,070 and 11,340 radio carbon years which would make them as old or older than discovered Clovis technology. Due to the age of the Western Stemmed projectile points, it means that humans entered what is now North America before the Ice Free corridor was open. This means that people likely arrived and made their way through North America by moving along the Pacific Coast. The age of the Western Stemmed projectile points also means that Western Stemmed technology and Clovis technology developed independently from each other and are not connected.',
             location: {
-                center: [-120.5579, 42.7529],
+                center: [-120.5527, 42.7072],
                 zoom: 6.0,
                 pitch: 60,
                 bearing: -63.00
@@ -100,7 +100,7 @@ var config = {
             alt: 'Human coprolites from the Paisley Caves',
             description: 'Source: Dr. Dennis LeRoy Jones - <a rel="nofollow" class="external free" href="https://osucascades.edu/sites/osucascades.edu/files/Jenkins.pdf">https://osucascades.edu/sites/osucascades.edu/files/Jenkins.pdf</a>, <a href="https://creativecommons.org/licenses/by-sa/4.0" title="Creative Commons Attribution-Share Alike 4.0">CC BY-SA 4.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=115683365">Link</a> <br><br>Coprolites are another important find at the Paisley Caves. Coprolites are fossilized poop, and can contain remains of partially digested materials. Coprolites at Paisley Caves give insight into what produced them, as well as what the environment was like at the time. Some of the coprolites were found to contain human DNA through fecal lipid biomarkers, which were dated to be around 12,400 radio carbon years. This finding shows that humans made the coprolites which predate clovis technology and provide further evidence that people arrived earlier than previously thought and through the Pacific Coastal route. Coprolites also contain pollen which can give insights into what the environment was like when the coprolites were made.',
             location: {
-                center: [-120.5579, 42.7529],
+                center: [-120.5527, 42.7072],
                 zoom: 6.5,
                 pitch: 60,
                 bearing: -43.2
@@ -120,7 +120,7 @@ var config = {
             alt: 'Map of Pleistocene lakes in the Great Basin',
             description: 'Source: By J. Havens - [drawn by J. Havens, U.S. Geological Survey, modified from figure 2 of Morrison, 1991 (Geological Society of America, Volume K-2, Quaternary Nonglacial Geology: Conterminous U.S.); first released by Reheis, M.C., and Bright, J., in 2009 on a now-defunct U.S.G.S. website; further modified by J. Havens in 2019].Note: present day Lake Abert incorrectly identified as “Albert”, <a href="https://creativecommons.org/licenses/by-sa/4.0" title="Creative Commons Attribution-Share Alike 4.0">CC BY-SA 4.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=89418876">Link</a><br><br>Pollen found in coprolites and sediment at Paisley Caves has provided an idea of what the environment was like in the Early Holocene and Late Pleistocene. The analysis of pollen found in coprolites shows that lakes and wetlands covered a much larger area and were much closer to the Paisley Caves, as well as more forested areas in the Cascade mountains. A shift in the Early Holocene also shows a shrinking of the lakes and an increase in wetlands. Pollen levels suggest short term seasonal occupation of the caves during the spring, summer, and fall. Pollen can be found in coprolites because when people breathe in, drink water, or consume plant materials they ingest pollen.',
             location: {
-                center: [-120.5579, 42.7529],
+                center: [-120.5527, 42.7072],
                 zoom: 5.5,
                 pitch: 10,
                 bearing: 0.00
@@ -138,7 +138,7 @@ var config = {
             title: 'What was learned from Paisley Caves',
             description: 'The findings at Paisley Caves have significantly contributed to our understanding of early human migration, technology and the environment in North America and the Great Basin during the Pleistocene and Early Holocene. The Western Stemmed projectile points and coprolites found at Paisley Caves have provided evidence for earlier human arrival and Pacific Coastal migration as an initial way for people to enter North America, as well as evidence that Western Stemmed points and Clovis technology developed independently from each other. The pollen in the human coprolites also provide insight into the paleoenvironment of the Great Basin and how the lakes used to be larger and when they started to dry there were more wetlands. The pollen also shows the seasonal occupation of the Paisley Caves and how the caves were occupied during the months with greater vegetation like the summer, spring, and fall. ',
             location: {
-                center: [-120.5579, 42.7529],
+                center: [-120.5527, 42.7072],
                 zoom: 5.5,
                 pitch: 0,
                 bearing: 0
